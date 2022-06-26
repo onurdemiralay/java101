b3852ae5ebd314a1582920c677664168e9302d7f
Onur DEMİRALAY <46560233+onurdemiralay@users.noreply.github.com>
2022-06-26 23:10:34 +0300

    Add files via upload

---
+++ etkinlikOnerme/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Sıcaklık: ");
+        heat = input.nextInt();
+
+        if (heat < 5){
+            System.out.print("Kayak yapabilirsiniz.");
+        }
+        else if (heat >= 5 && heat < 15) {
+            System.out.print("Sinemaya gidebilirsiniz.");
+        }
+        else if (heat >= 15 && heat < 25) {
+            System.out.print("Piknik yapabilirsiniz.");
+        }
+        else {
+            System.out.print("Yüzmeye gidebilirsiniz.");
+        }
+    }
+}
